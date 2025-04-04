cef58dec14ab26d6e5f757b54d41a454e4a47cbd
Gabriela Menendez <gabyvega2024@hotmail.com>
2025-04-03 19:22:43 -0600

    cambios3.0

--- L02P02_2022MV650_2022MV652/Models/libreriaDbContext.cs
+++ L02P02_2022MV650_2022MV652/Models/libreriaDbContext.cs
@@ -5,5 +5,14 @@ namespace L02P02_2022VF650_2022MV652.Models
 {
     public class libreriaDbContext : DbContext
     {
+        public libreriaDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<autores> autores { get; set; } 
+        public DbSet<libros> libros { get; set; }
+        public DbSet<comentarios_libros> comentarios_libros{ get; set; } 
+        
     }
 }
