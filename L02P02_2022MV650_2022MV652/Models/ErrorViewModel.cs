c12c6a35550133bb21d1cee089bd01a5a7cf6553
Gabriela Menendez <gabyvega2024@hotmail.com>
2025-04-03 18:16:22 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2022MV650_2022MV652/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022MV650_2022MV652.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
